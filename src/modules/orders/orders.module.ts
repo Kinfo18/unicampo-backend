c6b97a269ac191f83c195f93ee9432d4a7c5d6e4
David Leonardo Figueredo Manrique <64379701+Kinfo18@users.noreply.github.com>
2026-03-06 19:08:11 -0500

    fix: agregar PrismaService a providers de OrdersModule

--- src/modules/orders/orders.module.ts
+++ src/modules/orders/orders.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { OrdersController } from './orders.controller';
 import { OrdersService } from './orders.service';
+import { PrismaService } from '../../database/prisma.service';
 import { UsersModule } from '../users/users.module';
 
 @Module({
     imports: [UsersModule],
     controllers: [OrdersController],
-    providers: [OrdersService],
+    providers: [OrdersService, PrismaService],
 })
 export class OrdersModule {}
